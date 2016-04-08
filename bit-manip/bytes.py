ed96a6fa6bbd19d7930eeddc5614cb063779c9e0
Jack Fischer <jfische8@binghamton.edu>
2016-04-07 21:33:19 -0400

    working

--- bit-manip/bytes.py
+++ bit-manip/bytes.py
@@ -1,15 +1,17 @@
 #PYTHON 3
 import struct
+import os
 
 orig = open("video.mov", 'rb')
 new = open("new.mov", 'wb')
 
-#for b in orig.read():
-for i in range(10):
-  b = orig.read(1)
+size = os.fstat(orig.fileno()).st_size
+size = int(size/4)
+for _ in range(size):
+  b = orig.read(4)
   i = int.from_bytes(b, byteorder='big')
-  i = i + 69
-  new.write(i.to_bytes(1, byteorder='big'))
+  i = i +69
+  new.write(i.to_bytes(4, byteorder='big'))
 
 orig.close()
 new.close()
